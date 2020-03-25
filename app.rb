ca9f637f86e21b9384a93310fbd65fd374dac221
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-25 21:01:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,11 +37,15 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
+
+    
     "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
     #set session id here
+    session['id'] = 1
+    @session = session
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
